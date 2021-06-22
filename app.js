c3dd79b21c9b7019086af32ea8056630b01ff847
sirajeddine <sirajeddine.karboul123@gmail.com>
2021-06-22 12:12:46 +0100

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Siraj!'));
+app.get('/', (req, res) => res.send('Hello Siraj!how are you ?'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
